98bdf68de046bb1271905f1fee9430434a72f6d8
Michael Spaxman <seanforyou23@gmail.com>
2021-01-26 20:10:32 -0500

    [cleanup] update paths

--- init-draftcab.sh
+++ init-draftcab.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+DRAFTCAB_DATA_PATH="${HOME}/draftcab/modules/draftcab-ui/docker"
 
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: missing dependencies. Please run init-docker.sh and reboot.' >&2
   exit 1
 fi
 
-if ! $(id -nGz "$USER" | grep -qzxF "docker")
+if ! $(id -nGz "${USER}" | grep -qzxF "docker")
 then
   echo "User ${USER} does not belong to the docker group. Please run init-docker.sh or reboot if necessary."
   exit
@@ -25,14 +26,14 @@ fi
 
 echo "Cloning Draftcab..."
 cd ~/
-git clone git@github.com:draftcab/platform.git
+git clone git@github.com:draftcab/platform.git draftcab
 
 echo "Copying SSL certificate and relaxing permissions..."
-sudo cp -r ~/nginx-certbot/data/certbot ~/draftcab/docker/
-sudo chown -R ${USER} ~/draftcab/docker
+sudo cp -r ${HOME}/nginx-certbot/data/certbot ${DRAFTCAB_DATA_PATH}
+sudo chown -R ${USER} ${DRAFTCAB_DATA_PATH}
 
 echo "Building Draftcab docker image..."
-cd ~/draftcab
+cd ${HOME}/draftcab
 docker build . -t draftcab:dev
 
 echo "Starting Draftcab..."
